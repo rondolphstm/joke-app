af7424674f3e66a4ad4c96a3633907075cd0c8a4
rondolphstm <rondolphtst.m@gmail.com>
2022-11-08 16:19:10 -0500

    joke app created

---
+++ styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ff6",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  jokeText:{
+    textAlign: 'center',
+    fontSize: 40
+  },
+  punchline:{
+    textAlign: 'center',
+    color: '#c00',
+    fontSize: 32
+  },
+})
+
+export default styles
